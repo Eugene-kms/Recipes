9bfd92f4024773b791d39ed8f5114d735d9bbffe
Eugene-kms <ievgenkolomiiets@gmail.com>
2024-09-23 19:41:12 +0200

    Initial Commit

---
+++ Recipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recipes
+//
+//  Created by Eugene on 23.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
